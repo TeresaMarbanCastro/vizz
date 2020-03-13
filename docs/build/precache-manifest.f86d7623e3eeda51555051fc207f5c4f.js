1c00974ba45b29325dc1a8b4b9968af7ba9a5207
Teresa <teresamarbancastro@gmail.com>
2020-03-13 12:55:48 +0100

    publish build to deploy github pages

---
+++ docs/build/precache-manifest.f86d7623e3eeda51555051fc207f5c4f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735d4155f8a284897947b49c8818802d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76ffee2088503663f75c",
+    "url": "/static/css/main.a5b8bd95.chunk.css"
+  },
+  {
+    "revision": "49100cd11bbf6a934d83",
+    "url": "/static/js/2.5cbde81a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.5cbde81a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ffee2088503663f75c",
+    "url": "/static/js/main.d07a7325.chunk.js"
+  },
+  {
+    "revision": "62363abe6f087529efca",
+    "url": "/static/js/runtime-main.29fcbd12.js"
+  },
+  {
+    "revision": "a599f50b8f269629b081f7229d8c6db0",
+    "url": "/static/media/arrow-down.a599f50b.svg"
+  },
+  {
+    "revision": "9798c856b4b3cb2a8a5e6bf0492b2756",
+    "url": "/static/media/close.9798c856.svg"
+  },
+  {
+    "revision": "eba956a4440b8bbde23700fa5e90ffed",
+    "url": "/static/media/drag-dots.eba956a4.svg"
+  },
+  {
+    "revision": "edfe4042ea559f878b5d0e556b7cdbf4",
+    "url": "/static/media/hide-eye.edfe4042.svg"
+  },
+  {
+    "revision": "aed632c3ed87607e9459d4b47074b10f",
+    "url": "/static/media/info.aed632c3.svg"
+  },
+  {
+    "revision": "f503c7d400f78ca546a51e5a81390fc2",
+    "url": "/static/media/show.f503c7d4.svg"
+  }
+]);
